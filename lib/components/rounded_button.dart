bdbfb54917469c016440febfd492eac0ed86581b
unknown <famouszegenee@gmail.com>
2022-09-13 20:56:53 +0100

    added some animations and also refactored the codes

---
+++ lib/components/rounded_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+class RoundedButton extends StatelessWidget {
+  final String buttonText;
+  final Color buttonColor;
+  final Function onPressedButton;
+
+  RoundedButton({this.buttonText, this.buttonColor, @required
+  this.onPressedButton,
+  }) ;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: buttonColor,
+        //color: Colors.lightBlueAccent,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed:
+          //Go to login screen.
+          onPressedButton,
+          //Navigator.pushNamed(context, LoginScreen.id);
+
+
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+              buttonText
+            //'Log In',
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,18 +1,53 @@
 import 'package:flutter/material.dart';
+
+import 'package:animated_text_kit/animated_text_kit.dart';
+
 import 'package:my_app/screens/login_screen.dart';
 import 'package:my_app/screens/registration_screen.dart';
+import 'package:my_app/components/rounded_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
+//Animation settings start in WelcomeScreenState
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //specify the duration of the animation in the controller
+    controller = AnimationController(duration: Duration(seconds: 1),
+    //specify that the WelcomeScreenState should act as the picker
+    vsync: this,
+    );
+    //To animate from one color to another
+    animation = ColorTween(begin:Colors.blueGrey,  end: Colors.white).animate(controller);
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+    controller.forward();
+
+    controller.addListener(() {
+      setState(() {
+
+      });
+      print(animation.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      //ColoTween is used here to change the color from grey to white when the page is first loaded
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -21,65 +56,48 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                  ),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
+                //using the typewriter text kit here
+                TypewriterAnimatedTextKit(
+                  text:['Flash Chat'],
+                  textStyle: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
+                    color: Colors.black54
+
                   ),
+                  speed: Duration(milliseconds: 200),
+                  pause: Duration(microseconds: 1000),
                 ),
               ],
             ),
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to login screen.
-                    Navigator.pushNamed(context, LoginScreen.id);
+            RoundedButton(
+              buttonColor: Colors.lightBlueAccent,
+              buttonText:'Log In',
+              onPressedButton:(){Navigator.pushNamed(context, LoginScreen.id);
+              },),
+            RoundedButton(
+              buttonColor: Colors.blueAccent,
+              buttonText:'Register',
+              onPressedButton:() {
+                //Go to registration screen.
+                Navigator.pushNamed(context, RegistrationScreen.id);
 
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to registration screen.
-                    Navigator.pushNamed(context, RegistrationScreen.id);
+              },),
 
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
           ],
         ),
       ),
     );
   }
 }
+
